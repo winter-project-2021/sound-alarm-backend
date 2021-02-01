620e942eddafd2e3d192d8fed56b0f8b9fa6370a
yeonwookmo <moyu01@naver.com>
2021-02-01 22:40:55 +0900

    simplify codes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var fpcalc = require("fpcalc");
 var path = require('path')
 var multer= require('multer');
 const fs = require('fs');
-//var upload = multer( { dest: './public/download/' });
 var compare = require('./src/test.js');
 const { json } = require('express');
 const mongoose = require('mongoose');
@@ -16,24 +15,6 @@ var app = express ();
 var upload = multer();
 
 
-// var storage = multer.diskStorage({
-//     filename: function(req,file,cb){
-//         cb(null,file.fieldname+)
-//     }
-// })
-// const uri= process.env.ATLAS_URI;
-// mongoose.connect(uri,{
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true
-// })
-
-// const connection= mongoose.connection;
-
-// connection.once("open",()=>{
-//     console.log("MongoDB database connection success");
-// });
-
 app.listen(3000,function(){
     console.log("starting the server");
 })
@@ -43,30 +24,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false, limit: '1gb'}))
 
 
-
-// db.connect({
-//     host: process.env.DB_HOST,
-//     username: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD
-// });
-
-
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'./public/recorder.html'));
 });
 
 var time= new Date();
 
-// app.post('/',upload.single('music'),(req,res,next)=>{
-//     var obj={
-//         _id: 'hi',
-//         name: 'req.body.name',
-//         music:{
-//             data: fs.readFileSync(req.file.buffer)
-//         }
-//     }
-//     console.log(req.file)
-// })
+
 
 app.post('/ajax',upload.single('please'),function(req,res){
 
@@ -79,35 +43,3 @@ app.post('/ajax',upload.single('please'),function(req,res){
        
 
 })
-
-function makemp3(req){
-    try{
-        var process = new ffmpeg(req.file.path);
-        var name = './public/mp3/'+ req.file.filename+'.mp3';
-        //console.log(name,process);
-        process.then(function(audio){
-            audio.fnExtractSoundToMP3(name,function(error,file){
-                if(!error)
-                    console.log('Audio file: '+file);
-                    return name;
-            })
-        },function(err){
-            console.log("error: "+err);
-        });
-        console.log(process, 'later');
-    } catch(e) {
-        console.log(e);
-    }   
-}
-
-
-function fpcheck(file){
-    try{
-    fpcalc(file,{'raw':true},function(err,result){
-        console.log(result);
-    })
-    } catch(e){
-        console.log(e);
-    }
-}
-
