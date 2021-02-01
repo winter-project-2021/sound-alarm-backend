da61e3cf0779a27fcc0d4af104d9c8ac6775d34e
yeonwookmo <moyu01@naver.com>
2021-02-01 22:36:44 +0900

    simplify codes

--- src/test.js
+++ src/test.js
@@ -1,9 +1,9 @@
 var fpcalc = require("fpcalc");
-const WRONGDIGIT= 13
+const WRONGDIGIT= 15 // 감지 기준.
 const CRITERION= (32-WRONGDIGIT)/32*100
 var number1=0
 var number2=0
-var test=["./test/doorbell1.mp3"]
+var test=["./test/electronic.mp3"]
 
 
 compare= (number1,number2,name) => {
@@ -13,8 +13,8 @@ compare= (number1,number2,name) => {
     var num=0;
 
     for(j=0;j<number2.length;j++){
-     var bin=Math.abs((number1[i+j])^(number2[j])).toString(2)
-     num+=(bin.split('1').length-1)
+     var bin=Math.abs((number1[i+j])^(number2[j])).toString(2)//xor 연산후 이진법 스트링으로 변경
+     num+=(bin.split('1').length-1) // 1의 개수 세기
     }
 
       if(num<WRONGDIGIT*number2.length){
@@ -35,7 +35,7 @@ compare= (number1,number2,name) => {
 
   fpcalc(file1,{'raw':true},function(err, a) {
     if (err) throw err;
-    console.log('CRITERION ',CRITERION)
+    console.log('CRITERION ',CRITERION);
     number1=a.fingerprint;
     console.log('sample fp:',number1);
   
