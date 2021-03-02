d087daaa99bd39872a80f2e5d705bfd2028e1053
yeonwookmo <moyu01@naver.com>
2021-03-02 13:54:50 +0900

    server rap up

---
+++ node_modules/fpcalc/README.md
@@ -0,0 +1,68 @@
+# fpcalc
+
+This module is a wrapper around the [`fpcalc` command-line tool][chromaprint]
+and provides a node interface to calculate [AcoustID][] audio
+fingerprints for audio files.
+
+[chromaprint]: http://acoustid.org/chromaprint
+[acoustid]: http://acoustid.org/
+
+## Installing Chromaprint
+
+[`fpcalc` (provided by *Chromaprint*)][chromaprint] must be installed for
+this module to function.
+
+**OSX using Homebrew**
+
+```
+$ brew install chromaprint
+```
+
+**Ubuntu**
+
+```
+$ sudo apt-get install libchromaprint-tools
+```
+
+## Example
+
+```js
+var fpcalc = require("fpcalc");
+fpcalc("./audio.mp3", function(err, result) {
+  if (err) throw err;
+  console.log(result.file, result.duration, result.fingerprint);
+});
+```
+
+## API
+
+### `fpcalc(file, [options,] callback)`
+
+Calculates the fingerprint of the given audio file.
+
+*File* must be the path to an audio file or a readable stream.
+
+If using a stream, note that you will not get `duration` out due to an [fpcalc issue](https://github.com/acoustid/chromaprint/issues/53).
+
+*Options* may be an object with any of the following keys:
+
+ * `length`: Length of the audio data used for fingerprint calculation
+   (passed as `-length` option)
+ * `raw`: Output the raw uncompressed fingerprint (default: `false`)
+ * `command`: Path to the fpcalc command (default: `"fpcalc"` - expects
+   executable in `$PATH`)
+
+*Callback* must be a function that will be called with `callback(err,
+result)` once the fingerprint is calculated. The *result object* will
+contain the following keys:
+
+ * `file`: Path to the audio file
+ * `duration`: Duration of audio file in seconds
+ * `fingerprint`: Fingerprint of audio file - *Buffer* if `options.raw`,
+   *String* otherwise
+
+## Installation
+
+```
+npm install --save fpcalc
+```
---
+++ node_modules/fpcalc/test/test.js
@@ -0,0 +1,80 @@
+/* jshint node:true */
+"use strict";
+
+var path = require("path"),
+	fs = require("fs"),
+	test = require("tape"),
+	fpcalc = require("../");
+
+var TEST_FILE = path.join(__dirname, "test.mp3");
+
+test("get audio fingerprint", function(t) {
+	fpcalc(TEST_FILE, function(err, result) {
+		t.ifError(err);
+		t.ok(result.file);
+		t.ok(result.duration);
+		t.ok(result.fingerprint);
+		t.end();
+	});
+});
+
+test("bad file path", function(t) {
+	fpcalc("bad/path", function(err) {
+		t.ok(err);
+		t.end();
+	});
+});
+
+test("no file path", function(t) {
+	fpcalc(undefined, function(err) {
+		t.ok(err);
+		t.end();
+	});
+});
+
+test("non-audio file", function(t) {
+	fpcalc(path.join(__dirname, "/../index.js"), function(err) {
+		t.ok(err);
+		t.end();
+	});
+});
+
+test("fingerprint output", function(t) {
+	t.plan(5);
+
+	fpcalc(TEST_FILE, {raw: true}, function(err, result) {
+		t.ok(result.fingerprint);
+		t.ok(Buffer.isBuffer(result.fingerprint));
+	});
+
+	fpcalc(TEST_FILE, function(err, result) {
+		t.ok(result.fingerprint);
+		t.equal(typeof result.fingerprint, "string");
+		t.ok(/^[-_a-zA-Z0-9]+$/.test(result.fingerprint));
+	});
+});
+
+test("stream input", function(t) {
+	t.plan(5);
+
+	fpcalc(fs.createReadStream(TEST_FILE), {raw: true}, function(err, result) {
+		t.ok(result.fingerprint);
+		t.ok(Buffer.isBuffer(result.fingerprint));
+	});
+
+	fpcalc(fs.createReadStream(TEST_FILE), function(err, result) {
+		t.ok(result.fingerprint);
+		t.equal(typeof result.fingerprint, "string");
+		t.ok(/^[-_a-zA-Z0-9]+$/.test(result.fingerprint));
+	});
+});
+
+test("stream fignerprint is the same as file fingerprint", function(t) {
+	t.plan(1);
+
+	fpcalc(fs.createReadStream(TEST_FILE), function(err, streamResult) {
+		fpcalc(TEST_FILE, function(err, fileResult) {
+			t.equal(fileResult.fingerprint, streamResult.fingerprint);
+		});
+	});
+});
